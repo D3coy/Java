d151360b601d62fe3663acdba4e178bb6844935c
mr-ZA <zhanmo2@tutanota.com>
2020-03-30 00:18:48 -0800

    server and client not implements phone class

--- ClientServerSocket/src/Client.java
+++ ClientServerSocket/src/Client.java
@@ -14,31 +14,47 @@ public class Client
 		String host= "127.0.0.1";
 		int port = 8000;
 		
-		try (
-				Socket sock = new Socket(host, port);
-				
-				OutputStream streamW = sock.getOutputStream();
-				OutputStreamWriter streamWriter = new OutputStreamWriter(streamW);
-				BufferedWriter writer = new BufferedWriter(streamWriter);
-				
-				InputStream streamR = sock.getInputStream();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(streamR));
-				) {
-				
-			System.out.println("Connected to server succesfully!");
+		try (Phone phone = new Phone(host, port)){
+			System.out.println("Connected to server");
 			
 			String request = "Ufa";
-			System.out.println("Request_client: " + request);
+			System.out.println("Request: " + request);
+			phone.writeLine(request);
 			
-			writer.write(request);
-			writer.newLine();
-			writer.flush();
-			
-			String response = reader.readLine();
-			System.out.println("Response_server: " + response);
-		} 
-		catch (IOException e) {
-			e.printStackTrace();	// print every error from stack
+			String response = phone.readLine();
+			System.out.println("Response: " + response);
 		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+//		String host= "127.0.0.1";
+//		int port = 8000;
+//		
+//		try (
+//				Socket sock = new Socket(host, port);
+//				
+//				OutputStream streamW = sock.getOutputStream();
+//				OutputStreamWriter streamWriter = new OutputStreamWriter(streamW);
+//				BufferedWriter writer = new BufferedWriter(streamWriter);
+//				
+//				InputStream streamR = sock.getInputStream();
+//				BufferedReader reader = new BufferedReader(new InputStreamReader(streamR));
+//				) {
+//				
+//			System.out.println("Connected to server succesfully!");
+//			
+//			String request = "Ufa";
+//			System.out.println("Request_client: " + request);
+//			
+//			writer.write(request);
+//			writer.newLine();
+//			writer.flush();
+//			
+//			String response = reader.readLine();
+//			System.out.println("Response_server: " + response);
+//		} 
+//		catch (IOException e) {
+//			e.printStackTrace();	// print every error from stack
+//		}
 	}
 }
